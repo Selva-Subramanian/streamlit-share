e812b8fe94980eebcec716816bb22908d47547f9
SELVA SUBRAMANIAN S <ce19.selva@gmail.com>
2022-08-13 13:42:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # import the necessary packages
 import streamlit as st
+pip install streamlit-webrtc 
 from streamlit_webrtc import VideoTransformerBase
 from streamlit_webrtc import webrtc_streamer
 from scipy.spatial import distance as dist
